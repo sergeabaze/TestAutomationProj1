6b5db721219f309c34b64b7fc2e0714271d201a4
vmcgidev <vmcgidev@cgi-vm>
2020-05-11 22:40:42 +0000

    Add project files.

---
+++ TestAutomationProj1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestAutomationProj1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestAutomationProj1")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e9cc9f44-e664-4de7-875c-0c6ff90fc508")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TestAutomationProj1/UnitTest1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace TestAutomationProj1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+        [TestInitialize]
+        public void MyInitialize()
+        {
+
+        }
+
+        [TestCleanup]
+        public void Dispose()
+        {
+
+        }
+
+       // [TestProperty]
+        public void MyPropeerties()
+        {
+
+        }
+
+       // [TestClass]
+        public void MyTestClass()
+        {
+
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            ChromeOptions option = new ChromeOptions();
+            option.AddArgument("—start-maximized");
+            IWebDriver driver = new ChromeDriver(option);
+
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(30));
+            driver.Navigate().GoToUrl("https://www.google.lk/");
+            //var element = wait.Until(ExpectedConditions.ElementIsVisible(By.ClassName("gLFyf")));
+
+            //  wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditionsElementToBeClickable(By.Id("lst-ib")));
+            var element = wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.ClassName("gLFyf")));
+            IWebElement textField = driver.FindElement(By.ClassName("gLFyf"));
+            textField.SendKeys("Selenium");
+        }
+    }
+}
